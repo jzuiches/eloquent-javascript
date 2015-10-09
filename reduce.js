e14ab6e9d22c46978d4be55db08342241a8ac873
Joe Zuiches <jzuiches@yahoo.com>
2015-10-08 19:23:33 -0700

    reduce from scratch

--- reduce.js
+++ reduce.js
@@ -1,2 +1,12 @@
 var array = [1,2,3,4,5];
 
+function reduce(array, combine, start) {
+  var current = start;
+  for (var i = 0; i < array.length; i++)
+    current = combine(current, array[i]);
+  return current;
+}
+
+console.log(reduce(array), function(a, b){
+  return a+b;
+}, 0)
